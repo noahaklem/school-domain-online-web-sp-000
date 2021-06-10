02c30e7380472b52b4a1be7012e7646c128f4271
Noah  Klem <klem.noah@gmail.com>
2021-06-10 15:20:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+class School
+
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student_name, grade)
+    #is this 
+    if @roster.include?(grade)
+    #the same as
+    if roster.include?(grade)
+  
+  end 
+  
+
+  
+  
+  
+end
